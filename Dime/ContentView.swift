2cc45d931ac0e1b159d95fb930d15fd420a559d1
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-07-24 09:36:05 +0530

    Initial Commit

---
+++ Dime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dime
+//
+//  Created by Mrinmoy Borah on 24/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dime/DimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DimeApp.swift
+//  Dime
+//
+//  Created by Mrinmoy Borah on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
