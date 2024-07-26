77f32226e569b3d8d13dd2011c6676585e31a329
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-07-26 20:12:55 +0530

    linear gradient

---
+++ Dime/View/SendScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SendScreen.swift
+//  Dime
+//
+//  Created by Mrinmoy Borah on 26/07/24.
+//
+
+import SwiftUI
+
+struct SendScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SendScreen()
+}
