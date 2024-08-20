6e4c100905337217b08317859f394963f06eead0
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-08-20 23:14:03 +0530

    get spendings of user

--- Dime/APIService/spendAPIService.swift
+++ Dime/APIService/spendAPIService.swift
@@ -10,7 +10,7 @@ import Foundation
 class DimeSpendAPIService{
     
     // GET
-    func getSpendOfUser() async throws{
+    func getSpendOfUser() async throws -> [Spend]{
         let apiEndpoint: String = "https://localhost:3000/api/spend"
         
         guard let url = URL(string: apiEndpoint) else{
@@ -18,12 +18,17 @@ class DimeSpendAPIService{
         }
         
         let (data, response) = try await URLSession.shared.data(from: url)
-        guard let httpResponse = reponse as? HTTPURLResponse, httpResponse.statusCode ==200 else{
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else{
             dump(response)
             throw URLError(.badServerResponse)
         }
         
-        let urlData = try JSONDecoder
+        let urlData = try JSONDecoder().decode(getSpendModel.self, from: data)
+        let fetchedSpend: [Spend] = urlData.msg.flatMap{ msg->[Spend] in
+            return [msg]
+        }
+        print(fetchedSpend)
+        return fetchedSpend
     }
     
     
