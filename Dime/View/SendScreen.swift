5d1e25a8bf1d4c4e0b3c40451b9a521e4e437fd0
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-08-22 20:25:29 +0530

    Added numpad and send buttons in Send Payment Screen

--- Dime/View/SendScreen.swift
+++ Dime/View/SendScreen.swift
@@ -49,25 +49,40 @@ struct SendScreen: View {
                     DigitCard(number: "2", insertNumber: {viewModel.inputNumber("2")})
                     DigitCard(number: "3", insertNumber: {viewModel.inputNumber("3")})
                 }
-//                HStack(spacing: 80){
-//                    DigitCard(number: "4", insertNumber: viewModel.inputNumber("4"))
-//                    DigitCard(number: "5", insertNumber: viewModel.inputNumber("5"))
-//                    DigitCard(number: "6", insertNumber: viewModel.inputNumber("6"))
-//                }
-//                HStack(spacing: 80){
-//                    DigitCard(number: "7", insertNumber: viewModel.inputNumber("7"))
-//                    DigitCard(number: "8", insertNumber: viewModel.inputNumber("8"))
-//                    DigitCard(number: "9", insertNumber: viewModel.inputNumber("9"))
-//                }
-//                HStack(spacing: 80){
-//                    DigitCard(number: ".", insertNumber: viewModel.inputNumber("."))
-//                    DigitCard(number: "0", insertNumber: viewModel.inputNumber("0"))
-//                    DigitCard(number: "x", insertNumber: viewModel.inputNumber("x"))
-//                }
+                HStack(spacing: 80){
+                    DigitCard(number: "4", insertNumber: {viewModel.inputNumber("4")})
+                    DigitCard(number: "5", insertNumber: {viewModel.inputNumber("5")})
+                    DigitCard(number: "6", insertNumber: {viewModel.inputNumber("6")})
+                }
+                HStack(spacing: 80){
+                    DigitCard(number: "7", insertNumber: {viewModel.inputNumber("7")})
+                    DigitCard(number: "8", insertNumber: {viewModel.inputNumber("8")})
+                    DigitCard(number: "9", insertNumber: {viewModel.inputNumber("9")})
+                }
+                HStack(spacing: 80){
+                    DigitCard(number: ".", insertNumber: {viewModel.inputNumber(".")})
+                    DigitCard(number: "0", insertNumber: {viewModel.inputNumber("0")})
+                    DigitCard(number: "x", insertNumber: {viewModel.inputNumber("X")})
+                }
             }
             
             // Send Button
+            Spacer()
+                .frame(height: 50)
             
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                ZStack{
+                    Circle()
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(Color("MyPrimaryColor"))
+                    
+                    Image(systemName: "arrow.forward")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+
+            })
            Spacer()
             
             
